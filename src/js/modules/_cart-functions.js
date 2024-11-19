1e727fcf5dc2f56686a6722aea2dcae2ab6219c8
Egil Eskilsson <egil@egileskilsson.se>
2024-11-19 17:24:58 +0100

    Added: cart functions module with cartObject start structure

---
+++ src/js/modules/_cart-functions.js
@@ -0,0 +1,17 @@
+import { productsObject } from './_products-object.js';
+
+
+// cart object
+
+export const cartObject = {
+  cartSummary: {
+    subtotal: 0,
+    discounts: 0,
+    vat: 0,
+    shippingCost: 0,
+    total,
+  },
+};
+
+
+
