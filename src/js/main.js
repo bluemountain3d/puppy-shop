1792fb651184828d880b16b792ae6ab6e105b5ba
Egil Eskilsson <egil@egileskilsson.se>
2024-12-06 09:05:18 +0100

    Added: Functionality for theme switch

--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,16 @@ const homeBtn = document.querySelector('.js-home-btn');
 homeBtn.addEventListener('click', (e) => {
   location.reload(true);
 });
+
+const themeSwitchBtn = document.querySelector('.js-theme-switch');
+themeSwitchBtn.addEventListener('click', (e) => {
+  if (document.body.classList.contains('theme-dark')) {
+    document.body.classList.remove('theme-dark');
+    themeSwitchBtn.setAttribute('aria-label', 'Byt till ljust tema');
+    themeSwitchBtn.setAttribute('title', 'Byt till ljust tema');
+  } else {
+    document.body.classList.add('theme-dark');
+    themeSwitchBtn.setAttribute('aria-label', 'Byt till mörkt tema');
+    themeSwitchBtn.setAttribute('title', 'Byt till mörkt tema');
+  }
+});
